6f834a8cdf6a3f43ef1bef520c56d75ffa975386
emrhnfg <ffy_fatihg@hotmail.com>
2025-04-20 20:24:37 +0300

    Güncelleme yapıldı

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://fatihgunay:fatih2001810@cluster0.ubwxe.mongodb.net/food-del').then(()=>console.log("DB Connected"))
-}
